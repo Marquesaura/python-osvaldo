bf4c6dbfd8b39bb53575b3eb899d12939a9fa623
Laura Marques <161334577+Marquesaura@users.noreply.github.com>
2025-03-20 21:51:13 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import time 
 
+
 print("cronometro")
 horas = input("digite as horas ")
 horas  = int(horas)
@@ -11,28 +12,31 @@ segundos = int(segundos)
 
 iniciar = 0
 iniciar = int(iniciar)
-iniciar = input("Para começar, digite 1\nPara refazer, digite 2 ")
+
 tempo = "Tempo {}h: {}m :{}s".format(horas, minutos, segundos)
 
-def conferir():
-    if (segundos > 60):
+if (segundos > 60):
         print("digite um valor válido!")
-    elif (minutos > 60):
+elif (minutos > 60):
         print("digite um valor válido!")
-    elif (horas > 24):
+elif (horas > 24):
         print("digite um valor válido!")
-
-
-def comecar():
-    segundos, minutos, horas
-    if iniciar == 1:
-        comecar()
         
-    if iniciar == 2 :
-        print(tempo)
-    else:
-        print ("inválido!")
-        
+def ir(): 
+    global segundos, minutos, horas
     while segundos > 0:
         segundos = segundos - 1 
         print(tempo)
+        break
+
+iniciar = input("Para começar, digite 1\nPara refazer, digite 2 ")
+def comecar():
+    global segundos, minutos, horas
+    if iniciar == 1:
+        ir()
+    
+    elif iniciar == 2:
+        return tempo
+    else:
+        print ("inválido!")
+        return tempo
