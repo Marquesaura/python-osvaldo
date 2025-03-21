1df8a125ae0fc838cb477da6d5d417644aabba8e
Laura Marques <161334577+Marquesaura@users.noreply.github.com>
2025-03-21 08:19:11 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,42 +1,19 @@
 import time 
 
+def cronometro(horas, minutos, segundos):
+    total_segundos = horas * 3600 + minutos * 60 + segundos
 
-print("cronometro")
-horas = input("digite as horas ")
-horas  = int(horas)
-minutos = input("digite os minutos ")
-minutos = int(minutos)
-segundos = input("digite os segundos ")
-segundos = int(segundos)
+    for t in range(total_segundos, -1, -1):
+        horas, resto = divmod(t, 3600)
+        minutos, segundos = divmod(resto, 60)
+        print(f"{horas:02d}:{minutos:02d}:{segundos:02d}", end="\r")
+        time.sleep(1)
+    print("Tempo esgotado")
 
+print("Bem-vindo ao cronometro mais legal de todos!!")
+print("Digite um tempo para o seu cronometro:")
+horas = int(input("Horas: "))
+minutos = int(input("Minutos: "))
+segundos = int(input("Segundos: "))
 
-iniciar = 0
-iniciar = int(iniciar)
-
-tempo = "Tempo {}h: {}m :{}s".format(horas, minutos, segundos)
-
-if (segundos > 60):
-        print("digite um valor válido!")
-elif (minutos > 60):
-        print("digite um valor válido!")
-elif (horas > 24):
-        print("digite um valor válido!")
-        
-def ir(): 
-    global segundos, minutos, horas
-    while segundos > 0:
-        segundos = segundos - 1 
-        print(tempo)
-        break
-
-iniciar = input("Para começar, digite 1\nPara refazer, digite 2 ")
-def comecar():
-    global segundos, minutos, horas
-    if iniciar == 1:
-        ir()
-    
-    elif iniciar == 2:
-        return tempo
-    else:
-        print ("inválido!")
-        return tempo
+cronometro(horas, minutos, segundos)
